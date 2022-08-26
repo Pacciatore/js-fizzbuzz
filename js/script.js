e405ff0ebbea56b4e2d3568a271e18cfe02fe74b
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-08-26 10:12:44 +0200

    Refactor js

--- js/script.js
+++ js/script.js
@@ -11,49 +11,35 @@ for (let i = 0; i < maxElement; i++) {
     const buzzNumber = exerciseNumber % 5;
     const element = document.createElement('div');
 
+    element.className = 'col-0 m-3 fw-bold';
+    element.style.textAlign = 'center';
+    element.style.display = 'flex';
+    element.style.flexDirection = 'column';
+    element.style.justifyContent = 'center';
+
     // Uso del col-0 per poter customizzare le grandezze
 
     if (fizzNumber === 0 && buzzNumber === 0) {
         element.append('FizzBuzz');
-        element.className = 'col-0 tr-bg-fizz-buzz m-3 fw-bold';
-
-        element.style.textAlign = 'center';
-        element.style.display = 'flex';
-        element.style.flexDirection = 'column';
-        element.style.justifyContent = 'center';
+        element.className = element.classList + ' tr-bg-fizz-buzz';
 
 
         console.log('FizzBuzz');
     } else if (fizzNumber === 0) {
         element.append('Fizz');
-        element.className = 'col-0 tr-bg-fizz m-3 fw-bold';
-
-        element.style.textAlign = 'center';
-        element.style.display = 'flex';
-        element.style.flexDirection = 'column';
-        element.style.justifyContent = 'center';
+        element.className = element.classList + ' tr-bg-fizz';
 
 
         console.log('Fizz');
     } else if (buzzNumber === 0) {
         element.append('Buzz');
-        element.className = 'col-0 tr-bg-buzz m-3 fw-bold';
-
-        element.style.textAlign = 'center';
-        element.style.display = 'flex';
-        element.style.flexDirection = 'column';
-        element.style.justifyContent = 'center';
+        element.className = element.classList + ' tr-bg-buzz';
 
 
         console.log('Buzz');
     } else {
         element.append(exerciseNumber);
-        element.className = 'col-0 tr-bg-number m-3 fw-bold';
-
-        element.style.textAlign = 'center';
-        element.style.display = 'flex';
-        element.style.flexDirection = 'column';
-        element.style.justifyContent = 'center';
+        element.className = element.classList + ' tr-bg-number';
 
 
         console.log(exerciseNumber);
